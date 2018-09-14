d5ec22b45185d4ff6b2d7ccc5d7d03b75a51d519
Brent Miller <invent81@yahoo.com>
2018-09-14 14:39:51 -0400

    update

--- smartapps/smartthings/garage-presence-debian-heyu-v2.src/garage-presence-debian-heyu-v2.groovy
+++ smartapps/smartthings/garage-presence-debian-heyu-v2.src/garage-presence-debian-heyu-v2.groovy
@@ -6,14 +6,9 @@ definition(
     category: "Safety & Security",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png"
-) 
+)
 
-
-
-
-preferences {
-
-	 	
+preferences {	
 	
 	section("When a presence sensor arrives or departs this location..") {
 		input "presence", "capability.presenceSensor", title: "Which sensor?"        
@@ -26,47 +21,41 @@ preferences {
     section("x10code..") {
 		input "x10code", "x10code", title: "What code?"
 	}
-    
+    section("Garage door") {
+		input "garage_sensor", "capability.contactSensor", title: "Which sensor?"
+        }   
 }
 
-
 def installed() {
-	subscribe(presence, "presence", presenceHandler)
-    
+	subscribe(presence, "presence", presenceHandler) 
 }
 def updated() {
 	unsubscribe()
 	subscribe(presence, "presence", presenceHandler)
 }
-
 def presenceHandler(evt) {
 	if (evt.value == "present") {
 		log.debug "${presence.label ?: presence.name} has arrived at the ${location}"
-        	// Don't send a continuous stream of text messages           
-            
-
-			def deltaSeconds = 3
+        	// Don't send a continuous stream of text messages     
+            def deltaSeconds = 3
             def timeAgo = new Date(now() - (1000 * deltaSeconds))
 			def recentEvents = presence.eventsSince(timeAgo)
 				log.debug "Found ${recentEvents?.size() ?: 0} events in the last $deltaSeconds seconds"
-			def alreadySentSms = recentEvents.count { it.value && it.value == "present" } > 1
-                        
+			def alreadySentSms = recentEvents.count { it.value && it.value == "present" } > 1                        
 			//if (alreadySentSms) {
 				//log.debug "SMS already sent within the last $deltaSeconds seconds"
             	//sendSms(phone1, "Arrival, sms already sent ")
               if (garage_sensor.currentContact == "open") {
 				log.debug "Door already open"
-            	sendSms(phone1, "Door already open")
-                
+            	sendSms(phone1, "Door already open")                
 			} else {
-        		//sendSms(phone1, "${presence.label ?: presence.name} has arrived ${location}")
-                
+        		//sendSms(phone1, "${presence.label ?: presence.name} has arrived ${location}")                
                 pause(1000)
                 sendSms(phone1, "opening garage now")
                 pause(1000)
                 
            		def params = [
-    				uri: "https://api.milltrek.net:12125",
+    				uri: "http://vpn.milltrek.net:12125",
     				path: "/cgi-bin/bluelava/bluelava.cgi",
             		contentType: 'application/json',
                     query: [action:'on', device: "${x10code}"]
